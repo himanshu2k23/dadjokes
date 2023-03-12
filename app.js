d763561faf15914191f720836d52d93ccd38e5f9
himanshudubey2k23 <himanshudubey2k23@gmail.com>
2023-03-12 21:29:04 +0530

    Done

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 // VARIABLE 
 
-const button = document.querySelector('button');
+const button = document.querySelector('.btn');
+const ul = document.querySelector('ul');
 
 // FUNCTION 
-const addjoke = () => {
+
+const addJoke = async() => {
     //alert('it wokrs');
-    
+    let jokeText= await getJoke();
+    let newLi = document.createElement('li');
+    newLi.innerText= jokeText;
+    ul.appendChild(newLi);
 }
 
-
+const getJoke = async () => {
+    try {
+        let config = { headers: { Accept:"application/json" }};
+        let joke= await axios.get("https://icanhazdadjoke.com/",config);
+        return joke.data.joke;
+    }catch(e){
+        return e;
+    }
+}
 //EVENTS
-button.addEventListener('click', addjoke)
+
+button.addEventListener('click', addJoke)
